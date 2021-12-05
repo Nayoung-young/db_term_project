9693ff127baa40a852f459a12f567e9966e409cb
Nayoung-young <lina_ny@naver.com>
2021-12-05 16:22:51 +0900

    templates mock-up

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.templating import render_template
+from flask import render_template
 #from flask import render_template
 
 from db import testQuery # python 파일에서 함수 가져오기 
